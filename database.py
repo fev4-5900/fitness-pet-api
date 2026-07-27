8e3c015952177007a761e8cc96477a1cc451dd2d
azo5900 <azowael51@gmail.com>
2026-07-27 12:18:46 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+sqlalchemy_database_url = "sqlite:///./fitness-pet.db"
+
+engine = create_engine(sqlalchemy_database_url, connect_args={"check_same_thread": False})
+Session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
